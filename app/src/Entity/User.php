bf6897c1918f5ad81c40a2147926b35f142e5dbc
David Voda <voda.davd@gmail.com>
2022-01-19 21:54:32 +0100

    Registration

---
+++ app/src/Entity/User.php
@@ -0,0 +1,97 @@
+<?php
+
+// src/Entity/User.php
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\Doctrine\UuidGenerator;
+use Ramsey\Uuid\UuidInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ */
+#[ORM\Entity]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    public function __construct(
+        #[ORM\Id]
+        #[ORM\Column(type: 'uuid', unique: true)]
+        #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+        #[ORM\CustomIdGenerator(UuidGenerator::class)]
+        private UuidInterface $id,
+
+        #[ORM\Column(type: 'string', length: 180, unique: true)]
+        private string $email,
+
+        #[ORM\Column]
+        private string $password,
+
+        #[ORM\Column(type: 'json')]
+        private array $roles = [],
+    )
+    {
+    }
+
+    public function getId(): UuidInterface
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->email;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+}
